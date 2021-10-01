8e83d2b2c4928a3b7e2a9f5201bc0bc01afb433d
Fabián Tapia <art.loge@gmail.com>
2021-10-01 16:26:46 -0500

    v0.1

--- introduccionALaProgramacion/sketch01.java
+++ introduccionALaProgramacion/sketch01.java
@@ -2,8 +2,20 @@ class sketch01{
 	public static void main(String args[]){
 	
 	//TIPOS DE DATOS
-	System.out.println(450);
-	System.out.println(0.54);
+	int entero = 10;
+	double decimal = 9.5;
+	char caracter = 'c';
+	String cadena = "Cadena";
+	boolean boleano = true; //control lógico
+
+	System.out.println("Entero: " + entero);
+	System.out.println("Decimal: " + decimal);
+	System.out.println("Caracter: " + caracter);
+	System.out.println("Cadena" + cadena);
+	if(boleano)
+		System.out.println("Nuestro boleano es true");
+	if(boleano == true)
+		System.out.println("Y estamos muy seguros de eso");
 	
 	}
 }
